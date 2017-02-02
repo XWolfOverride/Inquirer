73fe5ac01fd3c5a66a74436184bc7ed9193e80fc
XWolf Override <xwolfoverride@gmail.com>
2017-02-02 16:51:50 +0100

    name fixup

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ MY_PATH="`dirname \"$0\"`"
 cd "$MY_PATH"
 echo "Starting minification"
 echo "//Inquirer Minified file (C) XWolfOverride"> inquirer.min.js
-echo "// MIT License and details on github">> inquirer.min.js
+echo "//MIT License and details on github">> inquirer.min.js
 curl -X POST -s --data-urlencode 'input@Merger.js' https://javascript-minifier.com/raw >> inquirer.min.js
 curl -X POST -s --data-urlencode 'input@Inquirer.js' https://javascript-minifier.com/raw >> inquirer.min.js
 echo Done!
