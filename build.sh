73458501ed01670da132b1334514c4bb2d718107
XWolfOverride <xwolfoverride@gmail.com>
2017-06-12 10:05:06 +0200

    Permanent log

--- build.sh
+++ build.sh
@@ -4,16 +4,17 @@
 MY_PATH="`dirname \"$0\"`"
 NOW=`date`
 cd "$MY_PATH"
-echo ">>> $NOW" >> inq.log.txt
-echo " Writing header" >> inq.log.txt
+echo ">>> $NOW" >> ../log/inquirer.txt
+echo " Writing header" >> ../log/inquirer.txt
 echo "Starting minification"
 echo "//Inquirer Minified file (C) XWolfOverride 2016"> inquirer.min.js
 echo "//MIT License and details on github https://github.com/XWolfOverride/Inquirer">> inquirer.min.js
 echo "//">> inquirer.min.js
-echo " Writing Merger.js" >> inq.log.txt
+echo " Writing Merger.js" >> ../log/inquirer.txt
 uglifyjs --compress --mangle -- Merger.js >> inquirer.min.js
-echo " Writing Inquirer.js" >> inq.log.txt
+echo " Writing Inquirer.js" >> ../log/inquirer.txt
 uglifyjs --compress --mangle -- Inquirer.js >> inquirer.min.js
 NOW=`date`
-echo "<<< $NOW" >> inq.log.txt
+echo "<<< $NOW" >> ../log/inquirer.txt
+ln -s /var/www/web/log/inquirer.txt inq.log.txt 
 echo Done!
