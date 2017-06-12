51ef024de8c5159666f3ffb022ef6ee16abb9cab
XWolfOverride <xwolfoverride@gmail.com>
2017-06-12 09:46:29 +0200

    Build log

--- build.sh
+++ build.sh
@@ -2,11 +2,19 @@
 #cat merger/core.js > inquirer-full.js
 #cat inquirer.js >>inquirer-full.js
 MY_PATH="`dirname \"$0\"`"
+NOW=`date`
 cd "$MY_PATH"
+echo "Starting publishment" >> inq.log.txt
+echo ">>> $NOW" >> inq.log.txt
+echo " Writing header" >> inq.log.txt
 echo "Starting minification"
 echo "//Inquirer Minified file (C) XWolfOverride 2016"> inquirer.min.js
 echo "//MIT License and details on github https://github.com/XWolfOverride/Inquirer">> inquirer.min.js
 echo "//">> inquirer.min.js
+echo " Writing Merger.js" >> inq.log.txt
 uglifyjs --compress --mangle -- Merger.js >> inquirer.min.js
+echo " Writing Inquirer.js" >> inq.log.txt
 uglifyjs --compress --mangle -- Inquirer.js >> inquirer.min.js
+NOW=`date`
+echo "<<< $NOW" >> inq.log.txt
 echo Done!
