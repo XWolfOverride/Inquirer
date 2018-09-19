757d6ccf669c1d96a4d06c081197c0d15432a0a3
XWolf Override <xwolfoverride@gmail.com>
2018-09-19 22:06:37 +0200

    Test

--- build.sh
+++ build.sh
@@ -2,19 +2,21 @@
 #cat merger/core.js > inquirer-full.js
 #cat inquirer.js >>inquirer-full.js
 MY_PATH="`dirname \"$0\"`"
-NOW=`date`
-cd "$MY_PATH"
-echo ">>> $NOW" >> ../log/inquirer.txt
-echo " Writing header" >> ../log/inquirer.txt
-echo "Starting minification"
-echo "//Inquirer Minified file (C) XWolfOverride 2016"> inquirer.min.js
-echo "//MIT License and details on github https://github.com/XWolfOverride/Inquirer">> inquirer.min.js
-echo "//">> inquirer.min.js
-echo " Writing Merger.js" >> ../log/inquirer.txt
-uglifyjs --compress --mangle -- Merger.js >> inquirer.min.js
-echo " Writing Inquirer.js" >> ../log/inquirer.txt
-uglifyjs --compress --mangle -- Inquirer.js >> inquirer.min.js
-NOW=`date`
-echo "<<< $NOW" >> ../log/inquirer.txt
-ln -s /var/www/web/log/inquirer.txt inq.log.txt 
-echo Done!
+echo "My Path: $MY_PATH"
+
+#NOW=`date`
+#cd "$MY_PATH"
+#echo ">>> $NOW" >> inquirer.log.txt
+#echo " Writing header" >> inquirer.log.txt
+#echo "Starting minification"
+#echo "//Inquirer Minified file (C) XWolfOverride 2016"> inquirer.min.js
+#echo "//MIT License and details on github https://github.com/XWolfOverride/Inquirer">> inquirer.min.js
+#echo "//">> inquirer.min.js
+#echo " Writing Merger.js" >> inquirer.log.txt
+#uglifyjs --compress --mangle -- Merger.js >> inquirer.min.js
+#echo " Writing Inquirer.js" >> inquirer.log.txt
+#uglifyjs --compress --mangle -- Inquirer.js >> inquirer.min.js
+#NOW=`date`
+#echo "<<< $NOW" >> inquirer.log.txt
+#ln -s /var/www/web/log/inquirer.txt inq.log.txt 
+#echo Done!
